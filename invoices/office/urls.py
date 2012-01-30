ba1ac3cb9f12fe3bf21b44485f4fd134982c662e
M. Jaszczyk <jaszczyk.michal@gmail.com>
2012-01-30 18:58:16 +0100

    Fix

--- invoices/office/urls.py
+++ invoices/office/urls.py
@@ -5,7 +5,7 @@ from __future__ import absolute_import
 from django.conf.urls import url, patterns
 
 from .views import index, render_form, get_choices, products_search,\
-        save_form, invoice_print, invoice_additional_info
+        save_form, render_invoice, invoice_additional_info
 
 base_invoice = '^formsave/(?P<invoice_type>\d)/'
 urlpatterns = patterns('',
@@ -15,7 +15,7 @@ urlpatterns = patterns('',
     url('%s/(?P<invoice_id>\d+)$/' % base_invoice, save_form),
     url('^choices/(?P<ct_id>\d+)/$', get_choices),
     url('^products/$', products_search),
-    url('^print/\.(?P<format>\w{2,4})$', invoice_print),
+    url('^print/\.(?P<format>\w{2,4})$', render_invoice),
     url('^additionalinfo/(?P<invoice_ids>[0-9,]+)$', invoice_additional_info),
 )
 
